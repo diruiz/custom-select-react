113d8682e60a558c2f81464efb2cd710e2fa6f6a
Diego Ruiz <diegoandres.ruiz@globant.com>
2021-10-21 10:44:28 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [isListOpen, setIsListOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
+  const onOptionClicked = value => () => {
+    setSelectedOption(value);
+    setIsListOpen(false);
+    console.log(selectedOption);
+};
+
+  const toggle = () => { setIsListOpen(!isListOpen) };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="main">
+        <h1>Custom select/dropdown</h1>
+        <div class="select-container">
+            <div class="select-header" onClick={toggle}> {selectedOption}</div>
+            { isListOpen && (
+              <ul class="select-style">
+                <li class="select-item" onClick={onOptionClicked('item 1')} >item 1</li>
+                <li class="select-item" onClick={onOptionClicked('item 2')} >item 2</li>
+                <li class="select-item" onClick={onOptionClicked('item 3')} >item 3</li>
+                <li class="select-item" onClick={onOptionClicked('item 4')} >item 4</li>
+                <li class="select-item" onClick={onOptionClicked('item 5')} >item 5</li>
+                <li class="select-item" onClick={onOptionClicked('item 6')} >item 6</li>
+                <li class="select-item" onClick={onOptionClicked('item 7')} >item 7</li>
+                <li class="select-item" onClick={onOptionClicked('item 8')} >item 8</li>
+                <li class="select-item" onClick={onOptionClicked('item 9')} >item 9</li>
+                <li class="select-item" onClick={onOptionClicked('item 10')} >item 10</li>
+              </ul>
+            )
+          }
+        </div>
     </div>
   );
 }
